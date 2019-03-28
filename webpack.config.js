984dba0929b6176def76ce0a3982034fa3b00a89
intumit <w24351789@gmail.com>
2019-03-28 12:16:28 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+let path = require('path')
+
+module.exports = {
+    entry: './js/script.js',
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js',
+        publicPath: './dist'
+    },
+}
